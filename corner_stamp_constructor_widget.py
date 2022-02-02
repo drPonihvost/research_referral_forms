7e51302476bc80e12d88083abdc08fa869066008
drPonihvost <kudrovpn@gmail.com>
2022-02-02 22:35:48 +0700

    Виджет конструктора элементов шаблона

---
+++ corner_stamp_constructor_widget.py
@@ -0,0 +1,19 @@
+from base_widgets import BaseWidget
+
+
+class CornerStampConstructor(BaseWidget):
+    def __init__(self, initiator):
+        super().__init__()
+        # layout
+
+        # widgets
+
+        # buttons
+
+        # configuration
+
+        # signals
+
+        # actions
+
+        # slots
